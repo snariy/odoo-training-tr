c69f0d72475161ba1a343c9c36094e982abab99d
snariy <senaarinur@gmail.com>
2019-09-03 15:39:33 +0300

    second day deneme

--- certification/__manifest__.py
+++ certification/__manifest__.py
@@ -12,12 +12,15 @@
     "license": "AGPL-3",
     "depends": ['base'],
     "data": [
+        'security/certification_security.xml',
         'security/ir.model.access.csv',
         'views/certification_view.xml',
         'views/res_partner_view.xml',
-        'views/standard_view.xml'
+        'views/standard_view.xml',
+        'views/certification_bodies.xml'
 
     ],
+    'demo': ['demo/certification_demo.xml'],
     'development_status':'Beta',
     'maintainers':['ceeficent'],
 }
--- certification/models/res_partner.py
+++ certification/models/res_partner.py
@@ -2,5 +2,5 @@ from odoo import models,fields
 
 class ResPartner(models.Model):
     _inherit='res.partner'
-certification_ids=fields.One2many(comodel_name='certificaiton',inverse_name='owner_id')
-is_certificaiton_body=fields.Boolean(string='It is an entity', default='True',help='Check this box if the concact is a ceritification entity')
+    certification_ids=fields.One2many(comodel_name='certification',inverse_name='owner_id')
+    is_certification_body=fields.Boolean(string='It is an entity', default='True',help='Check this box if the concact is a certification entity')
